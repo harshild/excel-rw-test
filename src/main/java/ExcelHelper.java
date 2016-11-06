9dce9ab3531d3acfcb1bcd13bbfb81ae20a69a4c
Harshil Dhariwal <harshil.dhariwal@gmail.com>
2016-11-06 14:26:01 +0530

    First

---
+++ src/main/java/ExcelHelper.java
@@ -0,0 +1,10 @@
+/**
+ * Created by geetika on 6/11/16.
+ */
+public class ExcelHelper {
+    private final int rowNumber;
+
+    public ExcelHelper(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+}
